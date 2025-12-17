929551fa049f26a2b43c02b77ca3ec2a9862042b
SpeedyGo55 <83915114+SpeedyGo55@users.noreply.github.com>
2025-12-17 09:05:56 +0100

    initital Commit

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"io/fs"
+	"fmt"
+	"log"
 )
 
 func main() {
 
 }
 
-func loadFile(path) {
+func loadFile(path string) {
+	file, err := fs.Open(path)
+	if err != nil {
+		return "", err
+	}
+	contents, err := ioutil.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+	return contents, nil
 }
